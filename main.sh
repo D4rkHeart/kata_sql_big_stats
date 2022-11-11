b5f0ea78681f49ca03c21cfd9e0a55b2c1864c66
D4rkHeart <leonardo.surdez@gmail.com>
2022-11-11 17:31:06 +0100

    [Feat] Script that run the main.py and the queries

--- main.sh
+++ main.sh
@@ -1,4 +1,8 @@
-.mode column
+#!/usr/bin/env sh
+python3 ./DataBase/mainV1.py;
+sqlite3 sensorRecord.db;
+.timeout 2000
+.mode column;
 SELECT COUNT(DISTINCT Name) AS "Nombre d'appareil" FROM Probe;
 SELECT Name AS "DEVICE",COUNT(Name) AS "CMPT" FROM Probe GROUP BY "DEVICE" ORDER BY "CMPT" DESC LIMIT 20;
 
