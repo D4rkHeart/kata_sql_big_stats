5fdec0668fe2c462da2d1a903f12e9161bbb52a8
D4rkHeart <leonardo.surdez@gmail.com>
2022-11-11 13:46:45 +0100

    [Feat] Querie for finding the number of distinct probe

--- DataBase/queries.sql
+++ DataBase/queries.sql
@@ -1,7 +1,8 @@
-SELECT
-  Name,
-  COUNT(Name) AS `count` 
+SELECT COUNT(DISTINCT Name) AS "Nombre d'appareil" FROM Probe;
+
+SELECT Name,COUNT(Name) AS "Nombre de réccurence"
 FROM Probe
 GROUP BY Name
-ORDER BY `count` DESC
+ORDER BY "Nombre de réccurence" DESC
 LIMIT 20;
+
