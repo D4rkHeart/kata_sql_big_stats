62aae4792b0d35843ecbf83ff9acc47de0b06d61
D4rkHeart <leonardo.surdez@gmail.com>
2022-11-16 15:01:06 +0100

    [Feat] Final Query - Work Done

--- DataBase/mainV1.py
+++ DataBase/mainV1.py
@@ -1,6 +1,5 @@
 import sqlite3
 from sqlite3 import Error
-
 # Create a connexion to the database
 def connectionCreation(dbName):
     connection = None
@@ -9,31 +8,23 @@ def connectionCreation(dbName):
     except Error as error:
         print(error)
     return connection
-
-
 def genericQuerie(connection, statement):
     # invoke methods that execute SQLite statements
     cursor = connection.cursor()
     # Execute the statement
     cursor.execute(statement)
-
 # Create the insert statement
-
-
 def insertInto(connection, statement):
     sqlStatement = '''INSERT INTO Probe(ID,Name,Value,Date) VALUES(?,?,?,?)'''
     # invoke methods that execute SQLite statements
     cursor = connection.cursor()
     # Execute the statement
     cursor.execute(sqlStatement, statement)
-
-
 def main():
     db = "sensorRecord.db"
     connection = connectionCreation(db)
     genericQuerie(connection, '''DROP TABLE IF EXISTS Probe''')
-    genericQuerie(
-        connection, '''CREATE TABLE Probe ( ID INTEGER PRIMARY KEY, Name TEXT NOT NULL, Value TEXT NOT NULL, Date TEXT NOT NULL )''')
+    genericQuerie(connection, '''CREATE TABLE Probe ( ID INTEGER PRIMARY KEY, Name TEXT NOT NULL, Value TEXT NOT NULL, Date TEXT NOT NULL )''')
     with connection:
         fullLine = ""
         db = open("./Data/states.csv")
@@ -47,6 +38,4 @@ def main():
                     contents[0][1:-1]), contents[2][1:-1], contents[3][1:-1], contents[7][1:-1]])
     connection.commit()
     print("Success ! All data are inserted")
-
-
 main()
--- queries.sql
+++ queries.sql
@@ -1,4 +1,5 @@
 .mode column
 SELECT COUNT(DISTINCT Name) AS "Nombre d'appareil" FROM Probe;
 SELECT Name AS "DEVICE",COUNT(Name) AS "CMPT" FROM Probe GROUP BY "DEVICE" ORDER BY "CMPT" DESC LIMIT 20;
-
+SELECT date("Date") AS "jour", COUNT(date("Date")) AS "CMPT" FROM Probe GROUP BY "jour" ORDER BY "CMPT" DESC;
+ 
